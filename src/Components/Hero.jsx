99ac2d8b4d3efff5d62847d8ddfc1c8089937f43
iamayushy <ayushhuns@gmail.com>
2022-07-30 20:26:34 +0530

    remove the gog

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,8 @@
-import flame from '../assets/fire.gif'
 const Hero = () => {
     return(
         <div className="hero">
             <section className="description">
-                <h1>We Strive To Make A Gamer Legend <p className="flame"> 🪙  </p></h1>
+                <h1>We Strive To Make A Gamer Legend <p className="flame">  🪙  </p></h1>
                 <button className="game-btn">Enter</button>
             </section>
         </div>
