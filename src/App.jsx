0e6d34f50e7cb8c08012cb81e50880799fbd8b67
iamayushy <ayushhuns@gmail.com>
2022-07-31 20:46:41 +0530

    completed the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { Compete } from "./Components/Compete"
 import { Game } from "./Components/Game"
 import { Hero } from "./Components/Hero"
 import { Navbar } from "./Components/Navbar"
+import { Reward } from "./Components/Rewards"
 import { Tape } from "./Components/Tape"
 
 
@@ -11,6 +13,8 @@ function App() {
       <Navbar/>
       <Hero/>
       <Game/>
+      <Reward/>
+      <Compete/>
       {/* <Tape/> */}
     </div>
   )
